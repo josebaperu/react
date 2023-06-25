d16fbbe5c31a1f49d0e27782c90595960fc2793c
jose <josetorresgarrido@gmail.com>
2023-06-24 23:20:22 -0500

    REACT : form.js

--- src/app/form/Form.js
+++ src/app/form/Form.js
@@ -1,12 +1,33 @@
+import React, { useState } from 'react';
+
 const Form = () => {
+    const [name, setName] = useState("defaultName");
+    const [age, setAge] = useState(0);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(age);
+        console.log(name);
+    }
     return (
-        <div className="Form">
-            <header className="Form-header">
-                <p>
-                    Form
-                </p>
-            </header>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>name:
+                <input
+                    type="text"
+                    placeholder={name}
+                    /*value={name} */
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+            <label>age:
+                <input
+                    type="number"
+                    placeholder={age}
+                    /*value={age} */
+                    onChange={(e) => setAge(e.target.value)}
+                />
+            </label>
+            <button type="submit" onSubmit={handleSubmit}>SUBMIT</button>
+        </form>
     );
 }
 export default Form;
