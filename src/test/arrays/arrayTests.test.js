07e40d1a6ab92f36da7281a0b4d76695b7a5392c
jose <josetorresgarrido@gmail.com>
2023-06-25 23:06:46 -0500

    react: jest setup

---
+++ src/test/arrays/arrayTests.test.js
@@ -0,0 +1,10 @@
+describe('arrayTests', () => {
+    test('test 1', () => {
+        expect((1 + 2)).toEqual(3);
+        expect((2 + 2)).toEqual(4);
+    });
+    test('test 2', () => {
+        expect((1 + 2)).toEqual(3);
+        expect((2 + 2)).toEqual(4);
+    });
+});
