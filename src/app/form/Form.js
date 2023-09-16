e00aa106222a8e6b9e0e1fc4e8b81dc22b8799d5
jose <josetorresgarrido@gmail.com>
2023-09-16 08:06:48 -0500

    react: redux

--- src/app/form/Form.js
+++ src/app/form/Form.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
-//import store from "../store/store";
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux'
 
 const Form = () => {
     const [name, setName] = useState("defaultName");
     const [age, setAge] = useState(0);
-    const dispatch = useDispatch()
+    const [persons, setPersons] = useState([]);
+    const dispatch = useDispatch();
+    const fetchPersons = async () => (await (await fetch("http://localhost:8080/api/person")).json());
+
+    useEffect(() => {
+        if(persons.length === 0) {
+            fetchPersons().then( data => {
+                setPersons(data);
+            });
+        }
+    },[persons]);
     return (
+        <>
         <form>
             <label>name:
                 <input
@@ -30,13 +40,30 @@ const Form = () => {
                     type: 'add',
                     item: {name, age}
                 });
-                /*store.dispatch({
-                    type: 'add',
-                    item: {name, age}
-                });*/
             }
             }>SUBMIT</button>
         </form>
+            {!!persons 
+            && 
+            <div>
+                <div>Persons</div>
+                <table>
+                    <tr>
+                        <th>id</th>
+                        <th>name</th>
+                    </tr>
+
+                {persons.map((p,k) => {
+                    return                     <tr>
+                        <td key={k}>{p.id}</td>
+                        <td key={k}>{p.name}</td>
+                    </tr>
+                })}
+                </table>
+             </div>
+            }
+
+        </>
     );
 }
 export default Form;
